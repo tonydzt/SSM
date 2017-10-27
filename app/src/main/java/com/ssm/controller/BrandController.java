826fdb7ba5e9b1edb3191daaa4207011b627b5a7
tony@egenie.cn <tony@egenie.cn>
2017-10-27 10:21:45 +0800

    test springmvc 自动匹配参数

--- app/src/main/java/com/ssm/controller/BrandController.java
+++ app/src/main/java/com/ssm/controller/BrandController.java
@@ -1,7 +1,9 @@
 package com.ssm.controller;
 
+import com.ssm.pojo.Brand;
 import com.ssm.service.IBrandService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -18,8 +20,20 @@ public class BrandController {
     @Resource
     private IBrandService brandService;
 
-    @RequestMapping("/showBrand")
-    public String showBrand() {
-        return brandService.getBrandById(100010L).toString();
+    //GET可以自动匹配
+    @RequestMapping(value = "/testGET", method = RequestMethod.GET)
+    public String testGET(Brand brand) {
+        System.out.println(brand.getBarCode());
+        System.out.println(brand.getBrandName());
+        return "success";
+    }
+
+    //POST好像不太能自动匹配,raw JSON类型不行,form-data和x-www-form-urlencode格式可以
+    //采购订单的查询方案request的Content-Type是application/x-www-form-urlencoded，所以可以获取到vo
+    @RequestMapping(value = "/testPOST")
+    public String testPOST(Brand brand) {
+        System.out.println(brand.getBarCode());
+        System.out.println(brand.getBrandName());
+        return "success";
     }
 }
